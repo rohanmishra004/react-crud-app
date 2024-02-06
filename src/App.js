f8d81faa94d4f0e259569ed0ae915ac2425167f4
Rohan Mishra <rohanmishra.004@gmail.com>
2024-02-06 12:35:02 +0530

    react tuts

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  // const [showText, setShowText] = useState(true)
+
+  const [counter, setCount] = useState(0)
+
+  const increaseCount = () => {
+    setCount(prevCount => prevCount + 1)
+  }
+
+  const decreaseCount = () => {
+    setCount(prevCount => prevCount > 0 ? prevCount - 1 : 0)
+  }
+
+  const setToZero = () => {
+    setCount(counter > 0 ? 0 : alert('counter already zero'))
+  }
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={increaseCount}>Increase</button>
+      <br />
+      <button onClick={decreaseCount}>Decrease</button>
+      <br />
+      <button onClick={setToZero}>SetToZero</button>
+      <br />
+
+      {counter}
+      {/* {showText === true && <p>Hi My name is Rohan</p>} */}
     </div>
   );
 }
