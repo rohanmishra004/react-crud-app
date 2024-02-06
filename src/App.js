61e9ba6c7a4d95e294c53b34ec56cb7e3a388359
Rohan Mishra <rohanmishra.004@gmail.com>
2024-02-06 12:42:04 +0530

    react basic setup done

--- src/App.js
+++ src/App.js
@@ -2,35 +2,10 @@ import './App.css';
 import { useState } from 'react';
 
 function App() {
-  // const [showText, setShowText] = useState(true)
-
-  const [counter, setCount] = useState(0)
-
-  const increaseCount = () => {
-    setCount(prevCount => prevCount + 1)
-  }
-
-  const decreaseCount = () => {
-    setCount(prevCount => prevCount > 0 ? prevCount - 1 : 0)
-  }
-
-  const setToZero = () => {
-    setCount(counter > 0 ? 0 : alert('counter already zero'))
-  }
-
-
 
   return (
     <div className="App">
-      <button onClick={increaseCount}>Increase</button>
-      <br />
-      <button onClick={decreaseCount}>Decrease</button>
-      <br />
-      <button onClick={setToZero}>SetToZero</button>
-      <br />
 
-      {counter}
-      {/* {showText === true && <p>Hi My name is Rohan</p>} */}
     </div>
   );
 }
